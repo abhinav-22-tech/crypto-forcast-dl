c318417fd67818b5151db2225d3c0f97cb8e28a5
Abhinav Jain <jainabhinav0860@gmail.com>
2025-11-02 16:23:42 +0530

    First model save

--- src/live_predict.py
+++ src/live_predict.py
@@ -4,13 +4,24 @@ import pandas as pd
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 
+from datetime import datetime, timedelta
+
+crypto = "BTC-INR"
+
 def get_live_data(ticker='BTC-INR', window=60):
-  data = yf.download(ticker, period="90d", interval="1d")
+
+  # Get today's date and subtract 1 day to exclude today's data
+  end_date = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')  
+  
+  
+  print(f"📡 Fetching latest data for {ticker}...")
+  data = yf.download(ticker, period="90d", interval="1d", end=end_date)
+
   if data.shape[0] == 0:
     raise ValueError("No data available to scale — check YahooFinance response.")
   
   data = data[['Close', 'High', 'Low', 'Open', 'Volume']]
-  print(data)
+  print(data.tail())
 
   # Feature Engineering
   data['MA7'] = data['Close'].rolling(window=7).mean()
@@ -44,7 +55,7 @@ def predict_next_price(model_path="models/price_lstm.keras"):
         np.concatenate([pred_scaled, np.zeros((pred_scaled.shape[0], scaled.shape[1]-1))], axis=1)
         )[0, 0]
   
-  last_close = data["Close"].tail(1).iloc[0]  
+  last_close = data["Close"]["BTC-INR"].iloc[-1] 
   print(f"📈 Last Close: {last_close}")
   print(f"🤖 Predicted Next Close: {pred_price}")
   return pred_price
@@ -53,7 +64,7 @@ if __name__ == "__main__":
   predict_next_price()
 
 
-#   📈 Last Close: Ticker
+# 📈 Last Close: Ticker
 # BTC-INR    9473145.0
 # Name: 2025-10-22 00:00:00, dtype: float64
 # 🤖 Predicted Next Close: 9599145.197542787
